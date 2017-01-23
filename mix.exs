5a57d9119f31512012cec4b84d91fca9b63f3ea9
Jonatan Männchen <jonatan@maennchen.ch>
2017-01-23 18:12:05 +0000

    Submit Coverage

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule DatatransHelper.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     test_coverage: [tool: ExCoveralls]]
   end
 
   # Configuration for the OTP application
