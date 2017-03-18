bd3d19459df171efa170147bdd7c0112f33748a6
Jonatan Männchen <jonatan@maennchen.ch>
2017-03-18 18:29:17 +0000

    Rename hmac_key to sign_hmac_key, Support Money Struct, Switch Amount to Integer

--- lib/datatrans_helper.ex
+++ lib/datatrans_helper.ex
@@ -1,4 +1,7 @@
 defmodule DatatransHelper do
+
+  @type datatrans_payment_request :: %{merchant_id: String.t, amount: float, currency: String.t, refno: String.t, sign: String.t}
+
   @moduledoc """
   Small Helper Function to sign Datatrans Request Parameters
   """
@@ -9,16 +12,15 @@ defmodule DatatransHelper do
   ### Examples
 
       iex> Application.put_env(:datatrans_helper, :merchant_id, "73452")
-      iex> Application.put_env(:datatrans_helper, :hmac_key, "your_key")
-      iex> DatatransHelper.generate_payment_info(7.2, "CHF", "a5e511e9-7334-44c2-be21-cef964091739")
-      %{amount: 7.2, currency: "CHF", merchant_id: "73452",
+      iex> Application.put_env(:datatrans_helper, :sign1_hmac_key, "your_key")
+      iex> DatatransHelper.generate_payment_info(7_20, "CHF", "a5e511e9-7334-44c2-be21-cef964091739")
+      %{amount: 7_20, currency: "CHF", merchant_id: "73452",
        refno: "a5e511e9-7334-44c2-be21-cef964091739",
-       sign: "1EC9627CC7BA2E58251656BD500672BB6C5509FD569BB31737EE381C56CFE785"}
+       sign: "2A28243478E60BF7CC5D418D99A00287173DA3963AA5B3459830DBDDB86EB648"}
 
   """
-  @spec generate_payment_info(float, String.t, String.t)
-    :: %{merchant_id: String.t, amount: float, currency: String.t, refno: String.t, sign: String.t}
-  def generate_payment_info(amount, currency, reference) do
+  @spec generate_payment_info(non_neg_integer, String.t, String.t) :: datatrans_payment_request
+  def generate_payment_info(amount, currency, reference) when amount > 0 do
     sign %{
       merchant_id: get_merchant_id(),
       amount: amount,
@@ -27,12 +29,39 @@ defmodule DatatransHelper do
     }
   end
 
-  @spec sign(%{merchant_id: String.t, amount: float, currency: String.t, refno: String.t})
+  if Code.ensure_compiled?(Money) do
+    @doc """
+    Generate Map of payment parameters.
+
+    This function is only present, if the optional `money` package is installed.
+
+    ### Examples
+
+        iex> Application.put_env(:datatrans_helper, :merchant_id, "73452")
+        iex> Application.put_env(:datatrans_helper, :sign1_hmac_key, "your_key")
+        iex> DatatransHelper.generate_payment_info(Money.new(7_20, :CHF), "a5e511e9-7334-44c2-be21-cef964091739")
+        %{amount: 7_20, currency: "CHF", merchant_id: "73452",
+         refno: "a5e511e9-7334-44c2-be21-cef964091739",
+         sign: "2A28243478E60BF7CC5D418D99A00287173DA3963AA5B3459830DBDDB86EB648"}
+
+    """
+    @spec generate_payment_info(Money.t, String.t) :: datatrans_payment_request
+    def generate_payment_info(%Money{amount: amount, currency: currency}, reference) when amount > 0 do
+      sign %{
+        merchant_id: get_merchant_id(),
+        amount: amount,
+        currency: Atom.to_string(currency),
+        refno: reference
+      }
+    end
+  end
+
+  @spec sign(%{merchant_id: String.t, amount: non_neg_integer, currency: String.t, refno: String.t})
     :: %{merchant_id: String.t, amount: float, currency: String.t, refno: String.t, sign: String.t}
   defp sign(payment_information) do
-    sign = Base.encode16 :crypto.hmac(:sha256, get_hmac_key(),
+    sign = Base.encode16 :crypto.hmac(:sha256, get_sign1_hmac_key(),
       payment_information[:merchant_id] <>
-      Float.to_string(payment_information[:amount]) <>
+      Integer.to_string(payment_information[:amount]) <>
       payment_information[:currency] <>
       payment_information[:refno]
     )
@@ -40,8 +69,8 @@ defmodule DatatransHelper do
     Map.put(payment_information, :sign, sign)
   end
 
-  @spec get_hmac_key() :: String.t
-  defp get_hmac_key, do: ConfigExt.fetch_env!(:datatrans_helper, :hmac_key)
+  @spec get_sign1_hmac_key() :: String.t
+  defp get_sign1_hmac_key, do: ConfigExt.fetch_env!(:datatrans_helper, :sign1_hmac_key)
 
   @spec get_merchant_id() :: String.t | integer
   defp get_merchant_id do
--- test/datatrans_helper_test.exs
+++ test/datatrans_helper_test.exs
@@ -7,34 +7,64 @@ defmodule DatatransHelperTest do
   doctest DatatransHelper
 
   @property_merchant_id pick_one(from: [string(min: 1), int(min: 1)])
-  @property_amount float(min: 0.1)
-  @property_currency string(min: 3, max: 3)
+  @property_amount positive_int()
+  @property_currency string(min: 3, max: 3, chars: :ascii)
   @property_reference string()
-  @property_hmac_key string(chars: ?A..?Z)
+  @property_sign1_hmac_key string(chars: ?A..?Z)
 
-  test "map has same output as config" do
-    ptest [
-      merchant_id: @property_merchant_id,
-      amount: @property_amount,
-      currency: @property_currency,
-      reference: @property_reference,
-      hmac_key: @property_hmac_key
-    ] do
-      Application.put_env(:datatrans_helper, :merchant_id, merchant_id)
-      Application.put_env(:datatrans_helper, :hmac_key, hmac_key)
+  describe "generate_payment_info/3" do
+    test "map has same output as config" do
+      ptest [
+        merchant_id: @property_merchant_id,
+        amount: @property_amount,
+        currency: @property_currency,
+        reference: @property_reference,
+        sign1_hmac_key: @property_sign1_hmac_key
+      ] do
+        Application.put_env(:datatrans_helper, :merchant_id, merchant_id)
+        Application.put_env(:datatrans_helper, :sign1_hmac_key, sign1_hmac_key)
 
-      payment_info = generate_payment_info(amount, currency, reference)
+        payment_info = generate_payment_info(amount, currency, reference)
 
-      assert payment_info[:merchant_id] == if is_integer(merchant_id), do: Integer.to_string(merchant_id), else: merchant_id
-      assert payment_info[:amount] == amount
-      assert payment_info[:currency] == currency
-      assert payment_info[:refno] == reference
-      assert payment_info[:sign] == :crypto.hmac(:sha256, hmac_key,
-        (if is_integer(merchant_id), do: Integer.to_string(merchant_id), else: merchant_id) <>
-        Float.to_string(amount) <>
-        currency <>
-        reference
-      ) |> Base.encode16
+        assert payment_info[:merchant_id] == if is_integer(merchant_id), do: Integer.to_string(merchant_id), else: merchant_id
+        assert payment_info[:amount] == amount
+        assert payment_info[:currency] == currency
+        assert payment_info[:refno] == reference
+        assert payment_info[:sign] == :crypto.hmac(:sha256, sign1_hmac_key,
+          (if is_integer(merchant_id), do: Integer.to_string(merchant_id), else: merchant_id) <>
+          Integer.to_string(amount) <>
+          currency <>
+          reference
+        ) |> Base.encode16
+      end
+    end
+  end
+
+  describe "generate_payment_info/2" do
+    test "map has same output as config" do
+      ptest [
+        merchant_id: @property_merchant_id,
+        amount: @property_amount,
+        currency: @property_currency,
+        reference: @property_reference,
+        sign1_hmac_key: @property_sign1_hmac_key
+      ] do
+        Application.put_env(:datatrans_helper, :merchant_id, merchant_id)
+        Application.put_env(:datatrans_helper, :sign1_hmac_key, sign1_hmac_key)
+
+        payment_info = generate_payment_info(%Money{amount: amount, currency: String.to_atom(currency)}, reference)
+
+        assert payment_info[:merchant_id] == if is_integer(merchant_id), do: Integer.to_string(merchant_id), else: merchant_id
+        assert payment_info[:amount] == amount
+        assert payment_info[:currency] == currency
+        assert payment_info[:refno] == reference
+        assert payment_info[:sign] == :crypto.hmac(:sha256, sign1_hmac_key,
+          (if is_integer(merchant_id), do: Integer.to_string(merchant_id), else: merchant_id) <>
+          Integer.to_string(amount) <>
+          currency <>
+          reference
+        ) |> Base.encode16
+      end
     end
   end
 end
